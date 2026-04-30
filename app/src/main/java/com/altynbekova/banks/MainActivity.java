92c506fd0930b6d0ee1711b1137a7b16ff0d3786
altynbekova <gulzhanaltynbekova@gmail.com>
2026-04-30 21:19:24 +0700

    initial commit

---
+++ app/src/main/java/com/altynbekova/banks/MainActivity.java
@@ -0,0 +1,33 @@
+package com.altynbekova.banks;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.altynbekova.banks.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        FragmentStateAdapter pageAdapter = new MyAdapter(this);
+        binding.pager.setAdapter(pageAdapter);
+    }
+}
